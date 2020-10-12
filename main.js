dea3f79290fd16cddb718086c8c32764306a5214
dojocoder523 <serg52396@yandex.ru>
2020-10-12 13:42:57 +0300

    Global Update

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ for (let i = 0; i < btn.length; i++) {
     }
 }
 
-let jrActive = document.querySelector('#journal');
+
 let equal = document.querySelector('.equal');
 equal.onclick = function () {
       let exp = text.value, exp1 = [];
@@ -34,66 +34,41 @@ equal.onclick = function () {
       } else {
           text.value += ' = ' + exp;
      
-        exp1.push(text.value);
-        jrActive.value += exp1 + '\n';
+        
+        
          
          
       }
         
 }
-let journal = document.querySelector('.jur'),
-panel = document.querySelector('#pan');
-journal.onclick = function () {
-   jrActive.classList.toggle('activeJur');
-   panel.classList.toggle('panActive');
-   text.classList.toggle('n1Active');
-}
 let clr = document.querySelector('.clr');
 clr.onclick = function () {
     text.value = '';
 }
-
 let bsp = document.querySelector('.backSpace');
 bsp.onclick = function () {
     let n = text.value;
     text.value = n.substring(0, n.length - 1);
 }
-let del = document.querySelector('.del');
-del.onclick = function () {
-    exp1 = '';
-    jrActive.value = exp1;
+
+let numPerc = document.querySelector('#number');
+let percent = document.querySelector('#percent');
+let result = document.querySelector('#result'),
+ok = document.querySelector('.ok');
+
+
+/* ok.onclick = function () {
+  let pd = numPerc.value*percent.value/100;
+  result.value = pd;
+} */
+
+let prcCalc = document.querySelector('#percentCalc'), prcBtn = document.querySelector('.prc'),
+clsPerc = document.querySelector('.clsPerc');
+
+prcBtn.onclick = function () {
+  prcCalc.classList.add('percAct');
 }
 
-let mBtn = document.querySelector('.menuBtn');
-let mMenu = document.querySelector('#mMenu');
-let cont = document.querySelector('#container');
-mBtn.onclick = function () {
-    mMenu.classList.add('menuActive');
-    cont.classList.add('conActive');
-}
-let clBtn = document.querySelector('.closeBtn');
-
-clBtn.onclick = function () {
-    mMenu.classList.remove('menuActive');
-    cont.classList.remove('conActive');
-}
-let setActive = document.querySelector('#settings'), setBtn = document.querySelector('.setAct'); 
-setBtn.onclick = function () {
-    setActive.classList.add('setActive');
-    
-}
-
-let setClose = document.querySelector('.setClose');
-
-setClose.onclick = function () {
-    setActive.classList.remove('setActive');
-}
-let about = document.querySelector('#aboutMe'),
-    abBtn = document.querySelector('.abAct'),
-    abClose = document.querySelector('.abClose');
-abBtn.onclick = function () {
-    about.classList.add('aboutAct');
-}
-abClose.onclick = function () {
-    about.classList.remove('aboutAct');
+clsPerc.onclick = function () {
+  prcCalc.classList.remove('percAct');
 }
